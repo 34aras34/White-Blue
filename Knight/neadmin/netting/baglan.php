c5765c4dcd730d5b34d6ba9b72678a171c7a7596
34aras34 <34321592+34aras34@users.noreply.github.com>
2017-12-07 02:05:59 +0300

    fdgd
    gfdgfd

--- Knight/neadmin/netting/baglan.php
+++ Knight/neadmin/netting/baglan.php
@@ -1,5 +1,6 @@
 <?php try {
-	$db=new PDO("mysql:host=localhost;dbname=bluee;charset=utf8", 'root','123aras123');
+	$db=new PDO("mysql:host=localhost;dbname=bluee;charset=utf8", '
+whitemmo_blueee','123aras123');
 	#echo "Başarıyla Veritabanına Bağlandınız";
 
 } catch (PDOException $e) {
