7a75f20cc0b8bf916dfd9c26699d65207e67409a
Çağlar Kabaca <elusiz070smurf@gmail.com>
2017-12-09 19:32:33 +0300

    asda
    asdasd

--- Knight/neadmin/header.php
+++ Knight/neadmin/header.php
@@ -71,7 +71,7 @@ session_start();
 
                 <a href="message-task.html" class="btn btn-info" title="New Message"><b>30 </b><i class="fa fa-envelope-o fa-2x"></i></a>
                 <a href="message-task.html" class="btn btn-primary" title="New Task"><b>40 </b><i class="fa fa-bars fa-2x"></i></a>
-                <a href="login.php" class="btn btn-danger" title="Logout"><i class="fa fa-exclamation-circle fa-2x"></i>Güvenli Çıkış</a>
+                <a href="logout.php" class="btn btn-danger" title="Logout"><i class="fa fa-exclamation-circle fa-2x"></i>Güvenli Çıkış</a>
 
             </div>
         </nav>
